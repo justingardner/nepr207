be349400de23199cba8b5eab6900fb30bca27a87
Dan Birman <danbirman@gmail.com>
2018-05-23 15:30:17 -0700

    Updating script to drop classification when flagged and to output decon files separately

--- 2018/classify_al.m
+++
@@ -1,5 +0,0 @@
-%%
-savedata_classification('/Users/dan/data/cogneuro/s036820180521',{'lV1','rV1'},2);
-savedata_classification('/Users/dan/data/cogneuro/s036920180521',{'lV1','rV1'},2);
-savedata_classification('/Users/dan/data/cogneuro/s037020180521',{'lV1','rV1'},2);
-savedata_classification('/Users/dan/data/cogneuro/s037120180521',{'lV1','rV1'},2);
--- 2018/savedata_classification.m
+++ 2018/savedata_classification.m
@@ -1,11 +1,11 @@
-function savedata_classification(cfolder,rois,scan)
+function savedata_classification(cfolder,rois,scan,analysis,classify)
 %%SAVEDATA_CLASSIFICATION
 %
 % Call:
-% savedata_classification('path/to/folder/',{'roi1','roi2'},scan#inConcatentation');
+% savedata_classification('path/to/folder/',{'roi1','roi2'},scan#inConcatentation,analysisName,pertrialforclassifiers);
 %
 % Example:
-% savedata_classification('/Users/dan/data/cogneuro/s037120180521',{'lV1','rV1'},2);
+% savedata_classification('/Users/dan/data/cogneuro/s037120180521',{'lV1','rV1'},2,'task',false);
 %
 % Output:
 % in folder 'path/to/folder/' you will find classify_roi1roi2.mat
@@ -13,6 +13,12 @@ function savedata_classification(cfolder,rois,scan)
 %
 %   resp (x*y*z * condition * time)
 %   roiresp (voxel * condition * time)
+%
+
+% Output folder default
+output_folder = '~/proj/nepr207/2018/';
+idx = strfind(cfolder,'s0');
+cf =  cfolder(idx:end);
 
 %% Pull conditions
 % Load from group 'Concatenation' scan # the event-related condition data.
@@ -43,14 +49,15 @@ view = newView();
 view = viewSet(view,'curGroup','Concatenation');
 view = viewSet(view,'curscan',scan);
 %% Get analysis
-view = loadAnalysis(view,sprintf('erAnal/%s','task')); % check analysis name!
+view = loadAnalysis(view,sprintf('erAnal/%s',analysis)); % check analysis name!
 
 
 %% Re-organize data matrix
 task = view.analyses{1};
 d = task.d{scan};
 
-data.resp = d.ehdr;
+% data.resp = d.ehdr; you can save out the full response matrix (entire
+% brain) using this, but per-roi is probably preferable
 data.roiresp = {};
 
 %% Get coordinates of rois
@@ -58,17 +65,32 @@ for ri = 1:length(rois)
     
     [scanCoords, ~] = getROICoordinates(view,rois{ri},scan,viewGet(view,'curGroup'),'straightXform',0);
 
-    data.roiresp{end+1} = zeros(size(scanCoords,2),size(data.resp,4),size(data.resp,5));
+    data.roiresp{end+1} = zeros(size(scanCoords,2),size(d.ehdr,4),size(d.ehdr,5));
     for vi = 1:size(scanCoords,2)
-        data.roiresp{end}(vi,:,:) = data.resp(scanCoords(1,vi),scanCoords(2,vi),scanCoords(3,vi),:,:);
+        data.roiresp{end}(vi,:,:) = d.ehdr(scanCoords(1,vi),scanCoords(2,vi),scanCoords(3,vi),:,:);
     end
     
-    carea = data.roiresp{end};
-    % save out individual matrices fort he areas
-    save(fullfile(cfolder,sprintf('classify_%s.mat',rois{ri})),'carea');
+%     % save out individual matrices for the areas (not necessary)
+%     carea = data.roiresp{end};
+%     save(fullfile(cfolder,sprintf('decon_%s.mat',rois{ri})),'carea');
 end
 %% Save
-save(fullfile(cfolder,sprintf('classify_%s',[rois{:}])),'data');
+save(fullfile(output_folder,cf,sprintf('decon_%s',[rois{:}])),'data');
+
+
+
+
+%% %%%%%%%%%%%%%%%%%
+%% Classification %%
+%% %%%%%%%%%%%%%%%%%
+
+if ~classify
+    % Return
+    mrQuit;
+    cd(cwd);
+    return
+end
+
 
 %% Get individual trial responses
 stimvol = d.stimvol;
@@ -84,35 +106,36 @@ for ri = 1:length(rois)
 end
 
 clear times
-for ti = 2
-    times{1} = getInstances(view,croi,stimvol,'startLag',ti,'blockLen',4,'n=inf');
-end
+for ti = 1:10
+        times{1} = getInstances(view,croi,stimvol,'startLag',ti,'blockLen',4,'n=inf');
 
-%% Pull instances and stack
+    %% Pull instances and stack
 
-for ri = 1:length(rois)
-    if length(times{1}{ri}.instance.instances)==2
-        numIns = size(times{1}{ri}.instance.instances{1},1)+size(times{1}{ri}.instance.instances{2},1);
-    else
-        numIns = size(times{1}{ri}.instance.instances{1},1)+size(times{1}{ri}.instance.instances{2},1)+size(times{1}{ri}.instance.instances{3},1);
-    end
-    data = zeros(length(times),numIns,size(times{1}{ri}.instance.instances{1},2));
-
-    for ti = 1:length(times)
-        croi = times{ti};
-        cond = [];
-        cdat = [];
-        for ci = 1:length(croi{ri}.instance.instances)
-            cond = [cond ; ci*ones(size(croi{ri}.instance.instances{ci},1),1)];
-            cdat = [cdat ; croi{ri}.instance.instances{ci}];
+    for ri = 1:length(rois)
+        if length(times{1}{ri}.instance.instances)==2
+            numIns = size(times{1}{ri}.instance.instances{1},1)+size(times{1}{ri}.instance.instances{2},1);
+        else
+            numIns = size(times{1}{ri}.instance.instances{1},1)+size(times{1}{ri}.instance.instances{2},1)+size(times{1}{ri}.instance.instances{3},1);
         end
-        data(ti,:,:) = cdat;
-        idx = strfind(cfolder,'s0');
-        cf =  cfolder(idx:end);
-        save(fullfile('~/proj/nepr207/2018/',cf,sprintf('%s_labels.mat',rois{ri})),'cond');
-        save(fullfile('~/proj/nepr207/2018/',cf,sprintf('%s_instances.mat',rois{ri})),'data');
+        data = zeros(numIns,size(times{1}{ri}.instance.instances{1},2));
+
+%         for ti = 1:length(times)
+            croi = times{1};
+            cond = [];
+            cdat = [];
+            for ci = 1:length(croi{ri}.instance.instances)
+                cond = [cond ; ci*ones(size(croi{ri}.instance.instances{ci},1),1)];
+                cdat = [cdat ; croi{ri}.instance.instances{ci}];
+            end
+            data(:,:) = cdat;
+
+            save(fullfile(output_folder,cf,sprintf('%i_%s_labels.mat',ti,rois{ri})),'cond');
+            save(fullfile(output_folder,cf,sprintf('%i_%s_instances.mat',ti,rois{ri})),'data');
+%         end
     end
+    
 end
+
 %% Return
 mrQuit;
 cd(cwd);
